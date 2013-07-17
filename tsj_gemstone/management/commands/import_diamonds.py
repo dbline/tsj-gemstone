011d7c9093d6ad0b06e83a2f9d0285901117d9a9
Matt McClanahan <matt@typethink.com>
2013-07-16 22:03:06 -0700

    Rearrange diamond importing.  REQUIRES MIGRATION
    Details:
     * Adds basic backend support
     * Add rapnet 1.0 support with better error logging
     * Add source, active (bool), and serial ID fields to diamond table
    
    The migration drops the diamond table, so import_diamonds needs to be
    run afterwards to repopulate the table.

--- tsj_gemstone/management/commands/import_diamonds.py
+++ tsj_gemstone/management/commands/import_diamonds.py
@@ -22,8 +22,14 @@ class Command(BaseCommand):
             default=prefs.get('rapaport_version', 'rapaport'),
             help='Backend to import from (rapaport, rapnet10) (default: value of rapaport_version pref)',
         ),
+        make_option('--async',
+            action='store_true',
+            dest='async',
+            help='Run this command asynchronously as a Celery task'
+        ),
     )
 
     def handle(self, *args, **options):
+        # TODO: Start Celery task if async=True
         backend = get_backend(options.get('backend'))
-        backend.main(filename=options.get('file'))
+        backend.Backend(filename=options.get('file')).run()

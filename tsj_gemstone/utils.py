c3a40874ddf26422718050eec6292d4289059649
Matt McClanahan <matt@typethink.com>
2014-10-11 22:30:43 -0700

    Add detect_backend for getting an import backend from a CSV/XLS header row

--- tsj_gemstone/utils.py
+++ tsj_gemstone/utils.py
@@ -1,3 +1,4 @@
+import csv
 from decimal import Decimal
 from importlib import import_module
 
@@ -7,6 +8,52 @@ def get_backend(mod):
     m = import_module('%s.%s' % (IMPORTER_MODULES_PACKAGE, mod))
 
     return m
+
+def _excel_header(fn):
+    # TODO: Move import to module level once xlrd is installed on all sites
+    import xlrd
+
+    wb = xlrd.open_workbook(file_contents=open(fn, 'rb').read())
+    sheet = wb.sheets()[0]
+    line = []
+    for col in range(sheet.ncols):
+        line.append(sheet.cell(0, col).value)
+    return line
+
+def _csv_header(fn):
+    reader = csv.reader(open(fn))
+    return reader.next()
+
+def get_header_row(fn):
+    if fn.lower().endswith('xls') or fn.lower().endswith('xlsx'):
+        row = _excel_header(fn)
+    if fn.lower().endswith('csv') or fn.lower().endswith('tsv'):
+        row = _csv_header(fn)
+
+    if row:
+        # Trim blank columns
+        row = [col for col in row if col]
+
+    return row
+
+def detect_backend(filename):
+    SHEET_TYPES = {
+        'emperor': ['DiamondID', 'Shape', 'Weight', 'Color', 'Clarity', 'askingPrice', 'Measurements', 'Cut', 'Lab', 'RapnetDiscountPercent', 'IndexPrice', 'IndexPriceDiscount', 'DepthPercent', 'TablePercent', 'GirdleMin', 'GirdleMax', 'GirdlePercent', 'GirdleCondition', 'CuletSize', 'CuletCondition', 'Polish', 'Symmetry', 'FluorescenceIntensity', 'FluorescenceColor', 'CrownHeight', 'CrownAngle', 'PavilionDepth', 'PavilionAngle', 'Enhancement', 'LaserInscription', 'FancyColor', 'FancyColorIntensity', 'FancyColorOvertone', 'Member Comments', 'Cert Comment', 'CertificateID', 'CertificateImage', 'ImageFile', 'SarinFile', 'VendorStockNumber', 'MatchingVendorStockNumber', 'IsMatchedPairSeparable', 'CityLocation', 'StateLocation', 'CountryLocation', 'ParcelStoneCount', 'Availability', 'TradeShow', 'ClientRowID'],
+        'gndiamond': ['SHAPE', 'SIZE', 'COLOR', 'CLARITY', 'CUT GRADE', 'PRICE', 'OFFRAPAPORT', 'CERT', 'DEPTH%', 'TABLE%', 'GIRDLE', 'CULET', 'POLISH', 'FLUORESCENCE INTENSITY', 'SYMMETRY', 'DiamondBrand', 'CROWN', 'PAVILION', 'MEASUREMENTS', 'COMMENT', 'STONES', 'CERT#', 'STOCK#', 'PAIR', ' PAIR SEPARABLE', 'FANCYCOLOR', 'TRADE SHOW', 'CERTIFICATE#', 'SHOWCERT', 'FancyMainColorBody'],
+        'hasenfeld': ['Shape', 'Size', 'Color', 'Clarity', 'Price Per Carat', 'LAB', 'Depth %', 'Table', 'Girdle', 'Culet', 'Polish', 'Symmetry', 'Fluorescence', 'Inventory', 'Measurements', 'Lab Number', 'Disc', 'Cut Grade', 'CertURL'],
+        'mkdiamonds': ['Shape', 'Weight', 'Color', 'Clarity', 'Measurements', 'Cut Grade', 'Lab', 'Price', 'Depth %', 'Table %', 'Girdle Thin', 'Girdle Thick', 'Girdle %', 'Culet Size', 'Culet Condition', 'Polish', 'Symmetry', 'Fluorescence Intensity', 'Fluorescence Color', 'Crown Height', 'Crown Angle', 'Pavilion Depth', 'Pavilion Angle', 'Treatment', 'LaserInscription', 'Comments', 'Certificate #', 'CertificateImage', 'VendorStockNumber', 'MatchedPairStockNumber', 'IsMatchedPairSeparable', 'FancyColor', 'FancyColorIntensity', 'FancyColorOvertone', 'ParcelStoneCount', 'TradeShow', 'Cash Price', 'Availability', 'RapLink', 'DiamondImage', 'City', 'State', 'Country'],
+        'rapaport': ['Lot #', 'Owner', 'Shape', 'Carat', 'Color', 'Clarity', 'Cut Grade', 'Price', '%/Rap', 'Cert', 'Depth', 'Table', 'Girdle', 'Culet', 'Polish', 'Sym', 'Fluor', 'Meas', 'Comment', '# Stones', 'Cert #', 'Stock #', 'Make', 'Date', 'City', 'State', 'Country', 'Image'],
+        'rapnet10': ['Seller', 'RapNet Seller Code', 'Shape', 'Weight', 'Color', 'Fancy Color', 'Fancy Intensity', 'Fancy Overtone', 'Clarity', 'Cut Grade', 'Polish', 'Symmetry', 'Fluorescence', 'Measurements', 'Lab', 'Cert #', 'Stock #', 'Treatment', 'RapNet Price', 'RapNet Discount Price', 'Depth %', 'Table %', 'Girdle', 'Culet', 'Comment', 'City', 'State', 'Country', 'Is Matched Pair Separable', 'Pair Stock #', 'Parcel number of stones', 'Certificate URL', 'RapNet Lot #', 'Date'],
+        'rdi': ['Allow RapLink Feed', 'Availability', 'Black Inclusion', 'Brand', 'Cash Discount %', 'Cash Price', 'Center Inclusion', 'Cert Comment', 'Certificate #', 'Certificate Filename', 'City', 'Clarity', 'Color', 'Country', 'Crown Angle', 'Culet Condition', 'Culet Size', 'Cut Grade', 'Depth %', 'Diamond Image', 'Display Cert Number', 'Fancy Color', 'Fancy Color Intensity', 'Fancy Color Overtone', 'Fluorescence Color', 'Fluorescence Intensity', 'Girdle %', 'Girdle Condition', 'Girdle Thick', 'Girdle Thin', 'Is Matched Pair Separable', 'Key To Symbols', 'Lab', 'Laser Inscription', 'MeasDepth', 'MeasLength', 'Measurements', 'MeasWidth', 'Member Comments', 'Pair Stock #', 'Parcel Stones', 'Pavilion Angle', 'Pavilion Depth', 'Polish', 'RapNet Discount %', 'RapNet Price', 'Report Issue Date', 'Report Issue Location', 'Report Type', 'Shade', 'Shape', 'ShowOnlyOnRapLink', 'Size', 'Star Length', 'State', 'Stock #', 'Symmetry', 'Table %', 'Trade Show', 'Treatment'],
+        'polygon': ['Supplier ID', 'Shape', 'Weight', 'Color', 'Clarity', 'Price / Carat', 'Lot Number', 'Stock Number', 'Lab', 'Cert #', 'Certificate Image', '2nd Image', 'Dimension', 'Depth %', 'Table %', 'Crown Angle', 'Crown %', 'Pavilion Angle', 'Pavilion %', 'Girdle Thinnest', 'Girdle Thickest', 'Girdle %', 'Culet Size', 'Culet Condition', 'Polish', 'Symmetry', 'Fluor Color', 'Fluor Intensity', 'Enhancements', 'Remarks', 'Availability', 'Is Active', 'FC-Main Body', 'FC- Intensity', 'FC- Overtone', 'Matched Pair', 'Separable', 'Matching Stock #', 'Pavilion', 'Syndication', 'Cut Grade', 'External Url'],
+        'varsha': [u'LOT NO', u'SHAPE', u'CARAT', u'COLOR', u'CLARITY', u'MEASUREMENT', u'DEPT ', u'TBL ', u'CUT', u'CUL', u'POL', u'SYM', u'FL', u'GIRDLE', u'LAB', u'PRICE / CT', u'TOTAL '],
+        'whitediamond': ['Shape', 'Carat', 'Color', 'Clarity', 'Measurements', 'Cut', 'Cert Type', 'Per Carat', 'Depth', 'Table', 'Girdle', 'Culet', 'Polish', 'Symmetry', 'Fluorescence', 'Cert Number', 'Cert Image', 'Stock Number', 'Supplier Name', 'Email', 'DiaImage'],
+    }
+
+    header = get_header_row(filename)
+    for format, row in SHEET_TYPES.items():
+        if header == row:
+            return format
 
 def moneyfmt(value, places=2, curr='$', sep=',', dp='.', pos='', neg='-', trailneg=''):
     """Convert Decimal to a money formatted string.

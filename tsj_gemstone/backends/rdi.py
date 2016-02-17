852e67ca6ec408bb8618cf4bed8c19898c09fdb2
Matt McClanahan <matt@typethink.com>
2016-02-17 04:32:47 +0000

    Add depth/table percent validation

--- tsj_gemstone/backends/rdi.py
+++ tsj_gemstone/backends/rdi.py
@@ -363,11 +363,15 @@ def write_diamond_row(line, cut_aliases, color_aliases, clarity_aliases, grading
 
     try:
         depth_percent = Decimal(str(clean(depth_percent)))
+        if depth_percent > 100:
+            raise InvalidOperation
     except InvalidOperation:
         depth_percent = 'NULL'
 
     try:
         table_percent = Decimal(str(cached_clean(table_percent)))
+        if table_percent > 100:
+            raise InvalidOperation
     except InvalidOperation:
         table_percent = 'NULL'
 

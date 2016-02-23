ea3bed94c60c7ca8307acef8013e7abcaa98cc08
Matt McClanahan <matt@typethink.com>
2016-02-23 13:55:03 -0800

    Add ASC to fieldsets

--- tsj_gemstone/prefs.py
+++ tsj_gemstone/prefs.py
@@ -36,9 +36,9 @@ class GemstonePreferencesForm(PreferencesForm):
             required=False, initial=True, help_text="Every imported diamond must be certified. If the certifier doesn't exist in the database, an entry will be automatically created by the import tool. If the diamond being imported isn't certified, it will be discarded. If the certifier of the diamond being imported exists but is disabled, it will be discarded.")
     rapaport_verify_cert_images = forms.BooleanField(label='Verify Cert. Images',
             required=False, help_text="If a certificate image URL is provided, confirm the URL. This will slow the import process down considerably as each defined certificate image is independently confirmed. If the image doesn't exist, the URL is removed from the diamond being imported but the rest of the diamond will be imported as expected.")
-    idex_access_key = forms.CharField(help_text="Your IDEX access key", required=False)
-    polygon_id = forms.CharField(help_text="Your Polygon ID", required=False)
-    asc = forms.CharField(help_text='FTP account for ASC', required=False)
+    idex_access_key = forms.CharField(label='IDEX Access Key', help_text="Your IDEX access key", required=False)
+    polygon_id = forms.CharField(label='Polygon ID', help_text="Your Polygon ID", required=False)
+    asc = forms.CharField(label='ASC Account', help_text='FTP account for ASC', required=False)
 
     gndiamond = forms.BooleanField(required=False, label='GN Diamond')
     hasenfeld = forms.BooleanField(required=False, label='Hasenfeld-Stein')
@@ -82,6 +82,11 @@ class GemstonePreferences(AppPreferences):
         (_('Polygon'), {
             'fields': (
                 'polygon_id',
+            ),
+        }),
+        (_('ASC'), {
+            'fields': (
+                'asc',
             ),
         }),
         (_('Additional feeds'), {

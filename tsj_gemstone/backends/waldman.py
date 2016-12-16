ee926fbe7f646f5341865f7a53c2c5e309190e6b
Matt McClanahan <matt@typethink.com>
2016-12-16 00:12:16 -0800

    Add waldman backend.  Prices are in CAD.

---
+++ tsj_gemstone/backends/waldman.py
@@ -0,0 +1,192 @@
+from decimal import Decimal, InvalidOperation
+import logging
+import os
+import re
+from string import ascii_letters, digits, whitespace, punctuation
+import tempfile
+
+from django.utils.lru_cache import lru_cache
+
+from .base import LRU_CACHE_MAXSIZE, XLSBackend, SkipDiamond, KeyValueError
+from .. import models
+from ..utils import moneyfmt
+
+logger = logging.getLogger(__name__)
+
+CLEAN_RE = re.compile('[%s%s%s%s]' % (punctuation, whitespace, ascii_letters, digits))
+
+def clean(data, upper=False):
+    if data is None:
+        return ''
+    data = ''.join(CLEAN_RE.findall(data)).strip().replace('\n', ' ').replace('\r', '')
+    if upper:
+        data = data.upper()
+
+    return data
+
+cached_clean = lru_cache(maxsize=LRU_CACHE_MAXSIZE)(clean)
+
+class Backend(XLSBackend):
+    debug_filename = os.path.join(os.path.dirname(__file__), '../tests/data/waldman.xlsx')
+    infile_glob = '/glusterfs/ftp_home/waldmancanadaftp/*xlsx'
+
+    def get_default_filename(self):
+        fn = max(glob.iglob(self.infile_glob), key=os.path.getctime)
+        if not fn:
+            raise ImportSourceError('No waldman file, aborting import.')
+
+        return fn
+
+    def write_diamond_row(self, line):
+        (
+            stock_number,
+            unused_origin,
+            cut,
+            carat_weight,
+            certifier,
+            color,
+            clarity,
+            cut_grade,
+            polish,
+            symmetry,
+            depth_percent,
+            table_percent,
+            length,
+            width,
+            depth,
+            carat_price,
+            unused_total_price,
+        ) = line
+
+        (
+            minimum_carat_weight,
+            maximum_carat_weight,
+            minimum_price,
+            maximum_price,
+            must_be_certified,
+            verify_cert_images
+        ) = self.pref_values
+
+        stock_number = clean(stock_number, upper=True)
+
+        try:
+            cut = self.cut_aliases[cached_clean(cut, upper=True)]
+        except KeyError as e:
+            raise KeyValueError('cut_aliases', e.args[0])
+
+        carat_weight = Decimal(str(cached_clean(carat_weight)))
+        if carat_weight < minimum_carat_weight:
+            raise SkipDiamond('Carat weight is less than the minimum of %s.' % minimum_carat_weight)
+        elif maximum_carat_weight and carat_weight > maximum_carat_weight:
+            raise SkipDiamond('Carat weight is greater than the maximum of %s.' % maximum_carat_weight)
+
+        color = self.color_aliases.get(cached_clean(color, upper=True))
+
+        certifier = cached_clean(certifier, upper=True)
+        # If the diamond must be certified and it isn't, raise an exception to prevent it from being imported
+        if must_be_certified:
+            if not certifier or certifier.find('NONE') >= 0 or certifier == 'N':
+                raise SkipDiamond('No valid certifier was specified.')
+        try:
+            certifier_id, certifier_disabled = self.certifier_aliases[certifier]
+        except KeyError as e:
+            raise KeyValueError('certifier_aliases', e.args[0])
+
+        if certifier_disabled:
+            raise SkipDiamond('Certifier disabled')
+
+        if certifier and not certifier_id:
+            new_certifier = models.Certifier.objects.create(name=certifier, abbr=certifier)
+            self.certifier_aliases.update({certifier: (int(new_certifier.id), new_certifier.disabled)})
+            certifier = new_certifier.pk
+        else:
+            certifier = certifier_id
+
+        clarity = cached_clean(clarity, upper=True)
+        if not clarity:
+            raise SkipDiamond('No clarity specified')
+        try:
+            clarity = self.clarity_aliases[clarity]
+        except KeyError as e:
+            raise KeyValueError('clarity', e.args[0])
+
+        cut_grade = self.grading_aliases.get(cached_clean(cut_grade, upper=True))
+        carat_price = clean(carat_price.replace(',', ''))
+        try:
+            carat_price = Decimal(carat_price)
+        except InvalidOperation:
+            carat_price = None
+
+        try:
+            depth_percent = Decimal(str(clean(depth_percent)))
+        except InvalidOperation:
+            depth_percent = 'NULL'
+
+        try:
+            table_percent = Decimal(str(cached_clean(table_percent)))
+        except InvalidOperation:
+            table_percent = 'NULL'
+
+        polish = self.grading_aliases.get(cached_clean(polish, upper=True))
+        symmetry = self.grading_aliases.get(cached_clean(symmetry, upper=True))
+
+        if carat_price is None:
+            raise SkipDiamond('No carat_price specified')
+
+        # Initialize price after all other data has been initialized
+        price_before_markup = carat_price * carat_weight
+
+        if minimum_price and price_before_markup < minimum_price:
+            raise SkipDiamond('Price before markup is less than the minimum of %s.' % minimum_price)
+        if maximum_price and price_before_markup > maximum_price:
+            raise SkipDiamond('Price before markup is greater than the maximum of %s.' % maximum_price)
+
+        price = None
+        for markup in self.markup_list:
+            if markup[0] <= price_before_markup and markup[1] >= price_before_markup:
+                price = (price_before_markup * (1 + markup[2]/100))
+                break
+        if not price:
+            raise SkipDiamond("A diamond markup doesn't exist for a diamond with pre-markup price of %s." % price_before_markup)
+
+
+        # Order must match struture of tsj_gemstone_diamond table
+        ret = self.Row(
+            self.added_date,
+            self.added_date,
+            't', # active
+            self.backend_module,
+            '', # lot_num
+            stock_number,
+            '', # owner,
+            cut,
+            self.nvl(cut_grade),
+            self.nvl(color),
+            clarity,
+            carat_weight,
+            moneyfmt(Decimal(carat_price), curr='', sep=''),
+            moneyfmt(Decimal(price), curr='', sep=''),
+            certifier,
+            '', #cert_num,
+            '', #cert_image,
+            '', #cert_image_local,
+            depth_percent,
+            table_percent,
+            '', #girdle,
+            '', #culet,
+            self.nvl(polish),
+            self.nvl(symmetry),
+            'NULL', #self.nvl(fluorescence_id),
+            'NULL', #self.nvl(fluorescence_color_id),
+            self.nvl(length),
+            self.nvl(width),
+            self.nvl(depth),
+            '', #comment,
+            '', #city,
+            '', #state,
+            '', #country,
+            'NULL', # rap_date
+            '{}', # data
+        )
+
+        return ret

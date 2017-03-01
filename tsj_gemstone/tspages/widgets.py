36188152cccea2c9890ddc7babce93c94c7614a3
Jacek Watala <jack.watala@typethink.com>
2017-03-01 14:46:30 +0000

    TSJ-956 | Added hidding shapes

--- tsj_gemstone/tspages/widgets.py
+++ tsj_gemstone/tspages/widgets.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.db.models import Q
 
 from ckeditor.widgets import CKEditorWidget
 
@@ -25,6 +26,7 @@ class GemstoneWidgetForm(PreferencesForm):
     header = forms.CharField(widget=CKEditorWidget(config_name='advanced'),
         required=False,
         help_text='Header content to put above the gemstones')
+    hide_gemstones=forms.MultipleChoiceField(label='Hide')
     show_view_all = forms.BooleanField(
         label='Show View All Button',
         required=False)
@@ -43,6 +45,19 @@ class GemstoneWidgetForm(PreferencesForm):
     template_name = forms.CharField(
 		required=False,
 		help_text='Custom template file, include path and name')
+
+    def __init__(self, *args, **kwargs):
+        super(GemstoneWidgetForm, self).__init__(*args, **kwargs)
+
+        cuts = models.Diamond.objects.values_list('cut', flat=True).order_by('cut__id').distinct()
+        if cuts:
+            qs = models.Cut.objects.filter(id__in=cuts)
+        else:
+            qs = models.Cut.objects.all()
+        
+        self.fields['hide_gemstones'].choices = [
+            (row[0], row[1]) for row in qs.values_list('pk','name')
+        ]
 
 class GemstoneWidget(TemplatedWidget):
     verbose_name = 'Gemstones'
@@ -73,6 +88,10 @@ class GemstoneWidget(TemplatedWidget):
             qs = models.Cut.objects.filter(id__in=cuts)
         else:
             qs = models.Cut.objects.all()
+
+        hide_gemstones = context['widget_style'] = self.preferences.get('hide_gemstones')
+        if hide_gemstones:
+            qs = qs.filter(~Q(id__in=hide_gemstones))
 
         for option in ('header', 'show_view_all', 'show_view_all_name', 'show_view_all_link', 'icon_style'):
             context[option] = self.preferences.get(option)

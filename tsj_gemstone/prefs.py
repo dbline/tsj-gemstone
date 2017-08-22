0e740d8eee532ad9805357b41b3483129dc720e1
Joel Ranck <joel@typethink.com>
2017-08-22 21:38:32 +0000

    Adding preference for overriding Sarine Template

--- tsj_gemstone/prefs.py
+++ tsj_gemstone/prefs.py
@@ -57,6 +57,8 @@ class GemstonePreferencesForm(PreferencesForm):
     show_prices = forms.ChoiceField(label='Show Prices to', choices=PRICE_CHOICES, help_text=_(u'Control how gemstone prices are shown on your website.'))
     group = forms.ModelChoiceField(queryset=Group.objects.all(), required=False, help_text='Limit prices to a specific group')
 
+    sarine_template = forms.CharField(label='Sarine Template', help_text='ID for overriding default Sarine Template', required=False)
+
     def clean_group(self):
         group = self.cleaned_data.get('group')
         if isinstance(group, Group):
@@ -98,6 +100,11 @@ class GemstonePreferences(AppPreferences):
         (_('ASC'), {
             'fields': (
                 'asc',
+            ),
+        }),
+        (_('Sarine'), {
+            'fields': (
+                'sarine_template',
             ),
         }),
         (_('Additional feeds'), {

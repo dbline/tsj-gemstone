bdd2810892725f327b1530e576ba27e85cc8156b
David Line <dbline@gmail.com>
2020-09-24 13:12:24 -0500

    Stuller API access URL changed to
    url = 'https://api.stuller.com/api/v2/gem'

--- tsj_gemstone/backends/stuller.py
+++ tsj_gemstone/backends/stuller.py
@@ -58,8 +58,7 @@ class Backend(JSONBackend):
         session = requests.Session()
         session.auth = (settings.STULLER_USER, settings.STULLER_PASSWORD)
         session.headers.update({'Content-Type': 'application/json', 'Accept': 'application/json'})
-        url = 'https://www.stuller.com/api/v2/gem'
-
+        url = 'https://api.stuller.com/api/v2/gem'
         next_page = None
         prev_page_hash = None
         serial_numbers = set()

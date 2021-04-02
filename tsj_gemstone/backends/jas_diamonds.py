fe847312b61b3e1680cf9984f62e2689e8627be5
David Line <dbline@gmail.com>
2021-04-01 21:04:34 -0500

    updated for automatic imports and additional backends.

--- tsj_gemstone/backends/jas_diamonds.py
+++ tsj_gemstone/backends/jas_diamonds.py
@@ -44,6 +44,13 @@ class Backend(CSVBackend):
     debug_filename = os.path.join(os.path.dirname(__file__), '../tests/data/jasdiamondslgd.csv')
     default_filename = os.path.join(settings.FTP_ROOT, 'jas-diamonds/jasdiamondslgd.csv')
 
+    @property
+    def enabled(self):
+        try:
+            return self.backend_module in prefs.get('backend')
+        except TypeError:
+            return False
+
     def write_diamond_row(self, line, blank_columns=None):
         if blank_columns:
             line = line[:-blank_columns]

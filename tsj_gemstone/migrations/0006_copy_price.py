0ea332ed3b4f1f54acffca3c1bdae20f047a61f6
Matt McClanahan <matt.mcclanahan@gmail.com>
2021-04-26 22:48:16 -0700

    Add using('default')

--- tsj_gemstone/migrations/0006_copy_price.py
+++ tsj_gemstone/migrations/0006_copy_price.py
@@ -7,7 +7,7 @@ def startend_to_minmax(apps, schema_editor):
     # We can't import the DiamondMarkup model directly as it may be a newer
     # version than this migration expects. We use the historical version.
     DiamondMarkup = apps.get_model('tsj_gemstone', 'DiamondMarkup')
-    for markup in DiamondMarkup.objects.all():
+    for markup in DiamondMarkup.objects.using('default').all():
         markup.minimum_price = markup.start_price
         markup.maximum_price = markup.end_price
         markup.save()
@@ -16,7 +16,7 @@ def minmax_to_startend(apps, schema_editor):
     # We can't import the DiamondMarkup model directly as it may be a newer
     # version than this migration expects. We use the historical version.
     DiamondMarkup = apps.get_model('tsj_gemstone', 'DiamondMarkup')
-    for markup in DiamondMarkup.objects.all():
+    for markup in DiamondMarkup.objects.using('default').all():
         markup.start_price = markup.minimum_price
         markup.end_price = markup.maximum_price
         markup.save()

843abf4ed55019ea4850a1e488c2a8a739523f75
David Line <david.line@typethink.com>
2021-10-12 20:16:15 +0000

    terrible hack to identify the differences between EDT and Stuller JSON backends

--- tsj_gemstone/backends/base.py
+++ tsj_gemstone/backends/base.py
@@ -384,9 +384,14 @@ class JSONBackend(BaseBackend):
         if not getattr(self, "partial_import", False):
             # Only mark active discontinued if we're running everything.
             models.Diamond.objects.filter(source=self.backend_module).update(active=False)
-
-        for obj in data:
-            self.try_write_row(writer, obj, existing_sns=existing_sns)
+        
+        # terrible hack to identify the differences between EDT and Stuller JSON backends
+        if hasattr(self, "partial_import"):
+            for obj in data:
+                self.try_write_row(writer, obj, existing_sns=existing_sns)    
+        else:
+            for obj in data:
+                self.try_write_row(writer, obj)
 
         if self.row_buffer:
             writer.writerows(self.row_buffer)
